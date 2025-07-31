6b1423517cfa18fb1988c503c808aa209569909e
NeoRemont <neoremont24@gmail.com>
2025-07-31 22:30:50 +0300

    Add files via upload

--- gpt.py
+++ gpt.py
@@ -1,11 +1,11 @@
 import os
-import openai
+from openai import OpenAI
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def ask_gpt(prompt):
-    response = await openai.ChatCompletion.acreate(
-        model=os.getenv("GPT_MODEL", "gpt-4-turbo"),
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
         messages=[{"role": "user", "content": prompt}]
     )
-    return response['choices'][0]['message']['content']
+    return response.choices[0].message.content
